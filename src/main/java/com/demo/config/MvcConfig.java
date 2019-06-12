6be4d0464c4de1907d121c114e1370bc7c87bc2a
tranhuuhongson <tranhuuhongson@gmail.com>
2019-06-12 19:48:41 +0700

    crawl website and divide fragment layout thymeleaf

--- src/main/java/com/demo/config/MvcConfig.java
+++ src/main/java/com/demo/config/MvcConfig.java
@@ -1,10 +1,7 @@
 package com.demo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
